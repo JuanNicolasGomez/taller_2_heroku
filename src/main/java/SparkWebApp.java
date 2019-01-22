540397ff661d6f5de351ed4faf79e82bbf924f3a
laboratorio laboratorio <laboratorio@linux-14.labinfo.is.escuelaing.edu.co>
2019-01-22 11:14:55 -0500

    html added

--- src/main/java/SparkWebApp.java
+++ src/main/java/SparkWebApp.java
@@ -9,7 +9,32 @@ public class SparkWebApp {
     public static void main(String[] args) {
         BasicConfigurator.configure();
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
+        get("/hello", (req, res) -> "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<title>Page Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h1>TALLER 2</h1>\n" +
+                "<p>web application to calculate the mean and standard deviation of a set of n\n" +
+                "real numbers.</p>\n" +
+                "\n" +
+                "<form action=\"/hello2\">\n" +
+                "  Enter the numbers separated by commas:<br>\n" +
+                "\n" +
+                "  <input type=\"text\" name=\"numbers\" value=\"213,234,34,21,5546,657\"><br>\n" +
+                "\n" +
+                "  <input type=\"submit\" value=\"Submit\">\n" +
+                "</form>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>");
+
+        get("/hello2", (req, res) -> {
+            return req.params();
+
+        });
     }
 
     static int getPort() {
